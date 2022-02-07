c5c08aeb1d621f5a3db14994651a48e43ea83973
LukNav <navasinskaslukas@gmail.com>
2022-02-08 00:03:36 +0200

    Modified Bullet script, added prototyping scene

--- Assets/1. Player/Scripts/BulletController.cs
+++ Assets/1. Player/Scripts/BulletController.cs
@@ -7,17 +7,18 @@ public class BulletController : MonoBehaviour
     public TrailRenderer trailRenderer;
     public float damage = 15f;
     public float selfDestroyTime = 2f;
+    public float selfDestroyDistance = 0.3f;
 
     private bool _isActive = false;
-    private Vector3 _lastPos;
-    private RaycastHit _hit;
+    private Vector2 _lastPos;
+    private RaycastHit2D _rayHit;
     //We don't want to destroy and instantiate bullets all the time - we want to enable and disable them, which is more effective
     private void OnEnable()
     {
         trailRenderer.Clear();
         Invoke("DestroySelf", selfDestroyTime);
         _isActive = true;
-        _lastPos = Vector3.down;
+        _lastPos = Vector2.down;
     }
 
     private void OnDisable()
@@ -28,40 +29,40 @@ public class BulletController : MonoBehaviour
 
     public void Update()
     {
+        _lastPos = new Vector2(transform.position.x, transform.position.y);
         if (_isActive)// PROBLEM <--- This activates when instantiating it - could affect the performance, could be optimised
         {
-            if (_lastPos == Vector3.down)
+            if (_lastPos == Vector2.down)
             {
                 _lastPos = transform.position;
                 return;
             }
 
-            Vector3 velocityDir = transform.position - _lastPos;
-            RaycastHit _hit;
+            Vector3 velocityDir =  - _lastPos;
+            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, velocityDir, selfDestroyDistance);
 
-            if (Physics.Raycast(transform.position, velocityDir, out _hit, 0.3f))
+            if (rayHit)//Convert this to 2D
             {
-                OnHit(_hit.collider, _hit);
+                OnHit(_rayHit.collider, rayHit);
             }
         }
     }
 
-    public void OnHit(Collider other, RaycastHit hit)
+    public void OnHit(Collider2D other, RaycastHit2D hit)
     {
         Debug.Log("Bullet HIT");
         StatsController statsController = other.GetComponentInParent<StatsController>();
         if (statsController != null)
         {
-            Vector3 hitDir =  transform.position - hit.point;//This direction is from the contact's perspective
+            Vector3 hitDir = _lastPos - hit.point;//This direction is from the contact's perspective
             statsController.TakeDamage(damage, hit.point, hitDir);
         }
         DestroySelf();
     }
 
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-    OnHit(collision.collider, _hit);
-
+        OnHit(collision.collider, _rayHit);
     }
 
 
