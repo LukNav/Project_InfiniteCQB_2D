9613489fa6d683c8f949aa43df7a2a07cacf51d0
LukNav <navasinskaslukas@gmail.com>
2022-03-08 17:38:56 +0200

    Added effects for health consumable pickup

--- Assets/8. Map/Scripts/Consumable.cs
+++ Assets/8. Map/Scripts/Consumable.cs
@@ -7,13 +7,25 @@ public class Consumable : MonoBehaviour
 {
     public float respawnDelay;
     public GameObject consumableBody;
-
+    private ParticleSystem _particleSystem;
     public delegate void HealthConsumableDelegate(float healAmount);
     public static HealthConsumableDelegate healthConsumableDelegate;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        if (!_particleSystem)
+            Debug.LogError("No Particle system was found");
+    }
     public void Trigger(Collider2D collision)
     {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+            return;
+
+        _particleSystem.Play();
         consumableBody.SetActive(false);
         Invoke("Respawn", 5f);
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
             healthConsumableDelegate(20f);
     }
